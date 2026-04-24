fe39bdc745e856eb69c0e55bc1ab30c2061a25ec
Brett Michelson <b.k.michelson27@gmail.com>
2026-04-24 09:53:32 -0500

    sdsds

--- src/components/master-layout/master-layout.ts
+++ src/components/master-layout/master-layout.ts
@@ -17,7 +17,7 @@ export class MasterLayout extends LitElement {
         return html`
         <div class="title-container">
             <img src=${patLogo}>
-            <a href="https://www.gofundme.com/f/supporting-the-ciarcia-family" target="_blank">Donate to the Ciarcia Family Here</a>
+            <a href="https://www.gofundme.com/f/supporting-the-ciarcia-family" target="_blank">Donate to the Ciarcia Family</a>
         </div>
 
         <div class="form-container">
