74f11d1e23d16de0ab5e3343baf254732b76ecbe
Brett Michelson <b.k.michelson27@gmail.com>
2026-04-24 20:33:38 -0500

    change environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ import type { Environment } from "./environment.model";
 let environmentToExport: Environment = {
     name: 'MOCKS',
     production: false,
-    apiBaseUrl: 'http://3.236.225.240:8080/silentauction'
+    apiBaseUrl: 'https://api.crmdynamics.click/silentauction'
 };
 
 export const environment = environmentToExport;
